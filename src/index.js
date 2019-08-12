3a89fc83cd14d2b639d0544c32931805d2f0cf36
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-08-12 21:00:06 +0200

    Commented-out deleted templated logo.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+// import logoImg from "./assets/logo.png";
 
 const config = {
   type: Phaser.AUTO,
@@ -15,18 +15,18 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+//   this.load.image("logo", logoImg);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+//   const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+//   this.tweens.add({
+//     targets: logo,
+//     y: 450,
+//     duration: 2000,
+//     ease: "Power2",
+//     yoyo: true,
+//     loop: -1
+//   });
 }
