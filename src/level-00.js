d24a331fb2bf4d51f9570155b84a10f52fc41d15
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-08-12 22:29:07 +0200

    Moved scene to its own file.

---
+++ src/level-00.js
@@ -0,0 +1,21 @@
+import { Scene } from 'phaser'
+
+class Level00 extends Scene {
+  preload () {
+    //   this.load.image("logo", logoImg);
+  }
+
+  create () {
+    //   const logo = this.add.image(400, 150, "logo");
+    //   this.tweens.add({
+    //     targets: logo,
+    //     y: 450,
+    //     duration: 2000,
+    //     ease: "Power2",
+    //     yoyo: true,
+    //     loop: -1
+    //   });
+  }
+}
+
+export default Level00
