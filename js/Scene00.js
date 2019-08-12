919209a9325076909c20969b51c638c6589745a0
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-08-12 20:35:23 +0200

    Refactored to StandardJS.

--- js/Scene00.js
+++ js/Scene00.js
@@ -1,11 +1,13 @@
+// import { Scene } from 'phaser'
+
 class Scene00 extends Phaser.Scene {
-  constructor() {
-    super("bootGame");
+  constructor () {
+    super('Scene00')
   }
 
-  create() {
-    this.add.text(20, 20, "Loading Game...");
+  create () {
+    this.add.text(20, 20, 'Loading Game...')
 
-    this.scene.start("playGame");
+    this.scene.start('playGame')
   }
 }
--- js/Scene01.js
+++ js/Scene01.js
@@ -1,30 +1,35 @@
+// import { Scene } from 'phaser'
+
 class Scene01 extends Phaser.Scene {
-  constructor() {
-    super("playGame");
+  constructor () {
+    super('playGame')
   }
 
-  preload() {
-    this.load.image("flag", "../assets/flag.png");
-    this.load.spritesheet("cells", "../assets/playfield/cells.png", {
-      frameWidth: 60,
-      frameHeight: 60
-    });
+  preload () {
+    const cellSize = 60
+    this.load.image('flag', '../assets/flag.png')
+    this.load.spritesheet('cells', '../assets/playfield/cells.png', {
+      frameWidth: cellSize,
+      frameHeight: cellSize
+    })
   }
 
-  create() {
-    this.add.text(20, 20, "Playing Game...", { font: "25px", fill: "green" });
+  create () {
+    this.add.text(20, 20, 'Playing Game...', { font: '25px', fill: 'green' })
 
-    this.flag = this.add.image(0, 0, "flag");
-    this.flag.setScale(0.5);
-    this.flag.setPosition(config.width / 2, this.game.config.height / 2);
+    this.flag = this.add.image(0, 0, 'flag')
+    this.flag.setScale(0.5)
+    this.flag.setPosition(
+      this.game.config.width / 2,
+      this.game.config.height / 2
+    )
 
     // Cells.
-    let cellSize = 60;
-    let cell = this.add.sprite(60, 60, "cells", 0);
-    cell.setInteractive();
+    const cell = this.add.sprite(60, 60, 'cells', 0)
+    cell.setInteractive()
 
     // Input events.
-    this.input.on("gameobjectup", this.cellClicked, this);
+    this.input.on('gameobjectup', this.cellClicked, this)
 
     // Moves the flag.
     // this.input.on(
@@ -37,9 +42,9 @@ class Scene01 extends Phaser.Scene {
     // );
   }
 
-  update() {}
+  update () {}
 
-  cellClicked(pointer, gameObject) {
-    gameObject.setFrame(9);
+  cellClicked (pointer, gameObject) {
+    gameObject.setFrame(9)
   }
 }
