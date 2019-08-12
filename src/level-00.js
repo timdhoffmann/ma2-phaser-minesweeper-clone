a2c3b7c0f10295050264de090e2523df88f5ea55
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2019-08-13 01:19:31 +0200

    Refactored image import and creation.

--- src/level-00.js
+++ src/level-00.js
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser'
 import { game } from './index'
+import flagImg from './assets/flag.png'
 
 class Level00 extends Scene {
   constructor () {
@@ -8,7 +9,7 @@ class Level00 extends Scene {
 
   preload () {
     const cellSize = 60
-    this.load.image('flag', 'src/assets/flag.png')
+    this.load.image('flag', flagImg)
     this.load.spritesheet('cells', 'src/assets/playfield/cells.png', {
       frameWidth: cellSize,
       frameHeight: cellSize
@@ -18,19 +19,20 @@ class Level00 extends Scene {
   create () {
     this.add.text(20, 20, 'Playing Game...', { font: '25px', fill: 'green' })
 
-    this.flag = this.add.image(0, 0, 'flag')
-    this.flag.setScale(0.5)
-    this.flag.setPosition(game.config.width / 2, game.config.height / 2)
+    const flag = this.add.image(0, 0, 'flag')
+    flag.setScale(0.5)
+    flag.setPosition(game.config.width / 2, game.config.height / 2)
 
-    // Cells.
-    const cell = this.add.sprite(60, 60, 'cells', 0)
-    cell.setInteractive()
+    this.createCells()
 
     // Input events.
     this.input.on('gameobjectup', this.cellClicked, this)
   }
 
-  update () {}
+  createCells () {
+    this.cell = this.add.sprite(60, 60, 'cells', 0)
+    this.cell.setInteractive()
+  }
 
   cellClicked (pointer, gameObject) {
     gameObject.setFrame(9)
